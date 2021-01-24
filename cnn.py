19b864dded8794e9814f37666cbce6b8165d2036
Oleg Kachalov <okachalov@icloud.com>
2021-01-24 10:32:45 +0300

    add args

--- cnn.py
+++ cnn.py
@@ -1,62 +1,72 @@
+import argparse
+import json
+
 import cv2
 import torch
 
 from model import Net
 
-cap = cv2.VideoCapture(f'video/videoplayback.mp4')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('CNN')
+    parser.add_argument('--config', type=str, default='./cnn_config.json')
+    parser.add_argument('--source', type=str, default='./video/videoplayback.mp4')
 
-d_w = 5
-d_h = 6
-digits = [[71, 12], [76, 12], [85, 12], [90, 12], [99, 12], [104, 12]]
+    args = parser.parse_args()
+    print(args)
 
-# [x_min, y_min, x_max, y_max]
-digit_areas = []
-for d in digits:
-    digit_areas.append([d[0], d[1], d[0] + d_w, d[1] + d_h])
+    config = json.loads(open(args.config, 'r').read())
+    print(config)
 
-model = Net()
-model.load_state_dict(torch.load('digit_classifier.pt', map_location='cpu'))
-model = model.eval()
+    cap = cv2.VideoCapture(args.source)
 
-trigger = False
+    # [x_min, y_min, x_max, y_max]
+    digit_areas = []
+    for d in config['digit_pos']:
+        digit_areas.append([d[0], d[1], d[0] + config['d_width'], d[1] + config['d_height']])
 
-while True:
-    ret, img = cap.read()
+    model = Net()
+    model.load_state_dict(torch.load(config['model'], map_location='cpu'))
+    model = model.eval()
 
-    if ret:
-        digit_img = []
-        for idx, d in enumerate(digit_areas):
-            tmp = img[d[1]:d[3], d[0]:d[2]]
-            tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
+    trigger = False
 
-            tmp = cv2.resize(tmp, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+    while True:
+        ret, img = cap.read()
 
-            trigger = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-            if trigger[0, 0] == 0:
-                tmp = cv2.bitwise_not(tmp)
+        if ret:
+            digit_img = []
+            for idx, d in enumerate(digit_areas):
+                tmp = img[d[1]:d[3], d[0]:d[2]]
+                tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
 
-            # cv2.imshow(f'{idx}', tmp)
+                tmp = cv2.resize(tmp, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
 
-            nn_tmp = torch.tensor(tmp, dtype=torch.float32)
-            nn_tmp = torch.div(nn_tmp, 255.0)
-            digit_img.append(nn_tmp.unsqueeze(0))
+                trigger = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+                if trigger[0, 0] == 0:
+                    tmp = cv2.bitwise_not(tmp)
 
-        digit_img = torch.stack(digit_img)
-        r = model(digit_img)
-        pred = torch.argmax(r, dim=1)
+                # cv2.imshow(f'{idx}', tmp)
 
-        zoom_img = img[8:20, 0:120, :]
+                nn_tmp = torch.tensor(tmp, dtype=torch.float32)
+                nn_tmp = torch.div(nn_tmp, 255.0)
+                digit_img.append(nn_tmp.unsqueeze(0))
 
-        time = f'{pred[0]}{pred[1]}:{pred[2]}{pred[3]}:{pred[4]}{pred[5]}'
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, time, (10, 200), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            digit_img = torch.stack(digit_img)
+            r = model(digit_img)
+            pred = torch.argmax(r, dim=1)
 
-        cv2.imshow('Frame', img)
-        cv2.imshow('Only for humans (zoom)', zoom_img)
-        k = cv2.waitKey()
+            zoom_img = img[8:20, 0:120, :]
 
-        if k == 113:
-            break
-    else:
-        print('Error read')
-        exit(1)
+            time = f'{pred[0]}{pred[1]}:{pred[2]}{pred[3]}:{pred[4]}{pred[5]}'
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(img, time, (10, 200), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            cv2.imshow('Frame', img)
+            cv2.imshow('Only for humans (zoom)', zoom_img)
+            k = cv2.waitKey()
+
+            if k == 113:
+                break
+        else:
+            print('Error read')
+            exit(1)
--- svm.py
+++ svm.py
@@ -1,56 +1,66 @@
+import argparse
+import json
+
 import cv2
 import numpy as np
 
 import pickle
 
-cap = cv2.VideoCapture(f'video/videoplayback.mp4')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('CNN')
+    parser.add_argument('--config', type=str, default='./svm_config.json')
+    parser.add_argument('--source', type=str, default='./video/videoplayback.mp4')
 
-d_w = 5
-d_h = 6
-digits = [[71, 12], [76, 12], [85, 12], [90, 12], [99, 12], [104, 12]]
+    args = parser.parse_args()
+    print(args)
 
-# [x_min, y_min, x_max, y_max]
-digit_areas = []
-for d in digits:
-    digit_areas.append([d[0], d[1], d[0] + d_w, d[1] + d_h])
+    config = json.loads(open(args.config, 'r').read())
+    print(config)
 
-clf = pickle.load(open('SVC.ski', 'rb'))
-trigger = False
+    cap = cv2.VideoCapture(args.source)
 
-while True:
-    ret, img = cap.read()
+    # [x_min, y_min, x_max, y_max]
+    digit_areas = []
+    for d in config['digit_pos']:
+        digit_areas.append([d[0], d[1], d[0] + config['d_width'], d[1] + config['d_height']])
 
-    if ret:
-        pred = []
-        for idx, d in enumerate(digit_areas):
-            tmp = img[d[1]:d[3], d[0]:d[2]]
-            tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
+    clf = pickle.load(open(config['model'], 'rb'))
+    trigger = False
 
-            tmp = cv2.resize(tmp, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
-            tmp = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 2)
+    while True:
+        ret, img = cap.read()
 
-            trigger = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-            if trigger[0, 0] == 0:
-                tmp = cv2.bitwise_not(tmp)
+        if ret:
+            pred = []
+            for idx, d in enumerate(digit_areas):
+                tmp = img[d[1]:d[3], d[0]:d[2]]
+                tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
 
-            # cv2.imshow(f'{idx}', tmp)
+                tmp = cv2.resize(tmp, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+                tmp = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 2)
 
-            tmp = np.true_divide(tmp, 255.0)
-            tmp = tmp.flatten().reshape(1, -1)
-            pred.append(int(clf.predict(tmp)[0]))
+                trigger = cv2.adaptiveThreshold(tmp, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+                if trigger[0, 0] == 0:
+                    tmp = cv2.bitwise_not(tmp)
 
-        zoom_img = img[8:20, 0:120, :]
+                # cv2.imshow(f'{idx}', tmp)
 
-        time = f'{pred[0]}{pred[1]}:{pred[2]}{pred[3]}:{pred[4]}{pred[5]}'
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, time, (10, 200), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                tmp = np.true_divide(tmp, 255.0)
+                tmp = tmp.flatten().reshape(1, -1)
+                pred.append(int(clf.predict(tmp)[0]))
 
-        cv2.imshow('Frame', img)
-        cv2.imshow('Only for humans (zoom)', zoom_img)
-        k = cv2.waitKey()
+            zoom_img = img[8:20, 0:120, :]
 
-        if k == 113:
-            break
-    else:
-        print('Error read')
-        exit(1)
+            time = f'{pred[0]}{pred[1]}:{pred[2]}{pred[3]}:{pred[4]}{pred[5]}'
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(img, time, (10, 200), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+            cv2.imshow('Frame', img)
+            cv2.imshow('Only for humans (zoom)', zoom_img)
+            k = cv2.waitKey()
+
+            if k == 113:
+                break
+        else:
+            print('Error read')
+            exit(1)
